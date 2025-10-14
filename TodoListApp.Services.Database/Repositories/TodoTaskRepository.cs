68e483f3a171fea318a5b88f6efa713ccc6c9ef1
OleksiiTymchenko <60030050+tta19764@users.noreply.github.com>
2025-10-14 15:12:03 +0300

    Revert "Site host"

--- TodoListApp.Services.Database/Repositories/TodoTaskRepository.cs
+++ TodoListApp.Services.Database/Repositories/TodoTaskRepository.cs
@@ -336,7 +336,6 @@ public class TodoTaskRepository : AbstractRepository, ITodoTaskRepository
             .Include(t => t.TaskTags)
                 .ThenInclude(tt => tt.Tag)
             .AsSplitQuery()
-                .OrderBy(t => t.Id)
             .Skip((pageNumber - 1) * rowCount)
             .Take(rowCount)
             .ToListAsync();
